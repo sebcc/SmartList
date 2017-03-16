352c37e10c7415e610b38b9aa092b67169291dca
Sebastien Caron <caron147@gmail.com>
2017-03-15 23:09:59 -0400

    Add unit tests project and create additemcommand test

---
+++ SmartList.Test/Commands/AddItemCommandTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Moq;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using PCLStorage;
+
+namespace SmartList.Test
+{
+    [TestFixture ()]
+    public class AddItemCommandTests
+    {
+
+        [Test ()]
+        public async Task Execute_AddItemNoOtherItems ()
+        {
+            // SETUP
+            var item = new Item () {
+                CategoryId = "catid",
+                Name = "Name1",
+                Id = Guid.NewGuid ().ToString ()
+            };
+            var fileStub = new FileStub (string.Empty);
+
+            var rootFolderMock = new Mock<IFolder> ();
+            var itemsFolderMock = new Mock<IFolder> ();
+            itemsFolderMock.Setup (ifm => ifm.GetFileAsync ("items.data", default (CancellationToken))).ReturnsAsync (fileStub);
+            rootFolderMock.Setup (rf => rf.CreateFolderAsync ("items", CreationCollisionOption.OpenIfExists, default (CancellationToken))).ReturnsAsync (itemsFolderMock.Object);
+
+            // ACTION
+            var called = false;
+            var command = new AddItemCommand (rootFolderMock.Object);
+            command.Added += (sender, e) => {
+                called = true;
+            };
+            command.Execute (item);
+
+
+            // ASSERT
+            Assert.IsTrue (called);
+            Assert.AreEqual (JsonConvert.SerializeObject (new List<Item> () { item }), fileStub.WrittenContent);
+        }
+    }
+}
---
+++ SmartList.Test/Stubs/FileStub.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using PCLStorage;
+
+namespace SmartList.Test
+{
+    public class FileStub : IFile
+    {
+        readonly string content;
+
+        public FileStub (string content)
+        {
+            this.content = content;
+        }
+
+        public string WrittenContent
+        {
+            get
+            {
+                var sr = new StreamReader (File.OpenRead ("test"));
+                return sr.ReadToEnd ();
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                throw new NotImplementedException ();
+            }
+        }
+
+        public string Path
+        {
+            get
+            {
+                throw new NotImplementedException ();
+            }
+        }
+
+        public Task WriteAllTextAsync (string contents)
+        {
+            return Task.FromResult<object> (null);
+        }
+
+        public Task<string> ReadAllTextAsync ()
+        {
+            return Task.FromResult (this.content);
+        }
+
+        public Task DeleteAsync (CancellationToken cancellationToken = default (CancellationToken))
+        {
+            throw new NotImplementedException ();
+        }
+
+        public Task MoveAsync (string newPath, NameCollisionOption collisionOption = NameCollisionOption.ReplaceExisting, CancellationToken cancellationToken = default (CancellationToken))
+        {
+            throw new NotImplementedException ();
+        }
+
+        public Task<Stream> OpenAsync (PCLStorage.FileAccess fileAccess, CancellationToken cancellationToken = default (CancellationToken))
+        {
+            if (fileAccess == PCLStorage.FileAccess.Read)
+            {
+                MemoryStream stream = new MemoryStream ();
+                StreamWriter writer = new StreamWriter (stream);
+                writer.Write (this.content);
+                writer.Flush ();
+                stream.Position = 0;
+                return Task.FromResult (stream as Stream);
+            }
+            else
+            {
+                return Task.FromResult (File.OpenWrite ("test") as Stream);
+
+            }
+        }
+
+        public Task RenameAsync (string newName, NameCollisionOption collisionOption = NameCollisionOption.FailIfExists, CancellationToken cancellationToken = default (CancellationToken))
+        {
+            throw new NotImplementedException ();
+        }
+    }
+}
